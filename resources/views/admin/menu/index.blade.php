8dfa23c4b8ea21a378cfe3c79dea475519ac5aba
RodrigoFuentesVera <51008797+RodrigoFuentesVera@users.noreply.github.com>
2019-05-27 12:17:24 -0400

    lista de menu

--- resources/views/admin/menu/index.blade.php
+++ resources/views/admin/menu/index.blade.php
@@ -1,43 +1,51 @@
 @extends("theme.$theme.layout")
-@section('titulo')
-Menú 
-@endsection
-
-@section('styles')
-<link rel="stylesheet" type="text/css" href="{{asset("assets/js/jquery-nestable/jquery.nestable.css")}}">
-@endsection
-
-@section('scriptsPlugins')
-<script src="{{asset("assets/js/jquery-nestable/jquery.nestable.js")}}" type="javascript"></script>
-@endsection
-
-@section('scripts')
-<script src="{{asset("assets/pages/scripts/admin/menu/index.js")}}" type="javascript"></script>
+@section("titulo")
+Menú
 @endsection
 
 @section('contenido')
 <div class="row">
     <div class="col-lg-12">
-        @include('includes.mensaje')
-        <div class="box box-info">
+        <div class="box box-success">
             <div class="box-header with-border">
-                <h3 class="box-title">Menús</h3>
+                <h3 class="box-title">Menús - Rol</h3>
             </div>
             <div class="box-body">
                 @csrf
-                <div class="dd" id="nestable">
-                    <ol class="dd-list">
-                        @foreach ($menus as $key => $item)
-                            @if ($item["id_padre"] != null)
-                                @break
-                            @endif
-                            @include("admin.menu.menu-item", ["item" => $item])
+                <table class="table table-striped table-bordered table-hover" id="tabla-data">
+                    <thead>
+                        <tr>
+                            <th>Menú</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($menus as $key => $menu)
+                        @if ($menu["id_padre"] != null)
+                            @continue
+                        @endif
+                            <tr>
+                                <td class="font-weight-bold"><i class="fa fa-arrows-alt"></i> {{$menu["nombre_menu"]}}</td>
+                            </tr>
+                            @foreach($menu["submenu"] as $key => $hijo)
+                                <tr>
+                                    <td class="pl-40"><i class="fa fa-arrow-right"></i> {{ $hijo["nombre_menu"] }}</td>
+                                </tr>
+                                @foreach ($hijo["submenu"] as $key => $hijo2)
+                                    <tr>
+                                        <td class="pl-80"><i class="fa fa-arrow-right"></i> {{$hijo2["nombre_menu"]}}</td>
+                                    </tr>
+                                    @foreach ($hijo2["submenu"] as $key => $hijo3)
+                                        <tr>
+                                            <td class="pl-120"><i class="fa fa-arrow-right"></i> {{$hijo3["nombre_menu"]}}</td>
+                                        </tr>
+                                    @endforeach
+                                @endforeach
+                            @endforeach
                         @endforeach
-                    </ol>
-                </div>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
-</div>
-    
+</div> 
 @endsection
